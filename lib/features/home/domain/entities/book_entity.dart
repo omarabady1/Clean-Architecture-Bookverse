72cff1fcbe279af201593f6edb87c09dfd48305a
omarabady <omarabady44@gmail.com>
2025-12-01 11:15:31 +0200

    Create BookEntity

---
+++ lib/features/home/domain/entities/book_entity.dart
@@ -0,0 +1,9 @@
+class BookEntity{
+  final String title;
+  final String description;
+  final String thumbnailUrl;
+  final String author;
+  final String previewUrl;
+
+  BookEntity({required this.title, required this.description, required this.thumbnailUrl, required this.author, required this.previewUrl});
+}
