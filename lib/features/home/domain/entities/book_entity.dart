e20cb831884a6423a09af13d4973ebfc0fdf4530
omarabady <omarabady44@gmail.com>
2025-12-01 18:50:01 +0200

    Create Book Model extends Book Entity

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,9 +1,17 @@
-class BookEntity{
+class BookEntity {
   final String title;
   final String description;
   final String thumbnailUrl;
   final String author;
   final String previewUrl;
+  final num pageCount;
 
-  BookEntity({required this.title, required this.description, required this.thumbnailUrl, required this.author, required this.previewUrl});
+  BookEntity({
+    required this.title,
+    required this.description,
+    required this.thumbnailUrl,
+    required this.author,
+    required this.previewUrl,
+    required this.pageCount,
+  });
 }
