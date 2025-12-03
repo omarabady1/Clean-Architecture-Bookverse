cdeedbe01047e420252191b024ffc02547806965
omarabady <omarabady44@gmail.com>
2025-12-03 13:35:44 +0200

    Add Hive and Generate BookEntity Type Adapter

--- lib/features/home/domain/entities/book_entity.dart
+++ lib/features/home/domain/entities/book_entity.dart
@@ -1,9 +1,18 @@
+import 'package:hive/hive.dart';
+part 'book_entity.g.dart';
+@HiveType(typeId: 0)
 class BookEntity {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String description;
+  @HiveField(2)
   final String thumbnailUrl;
+  @HiveField(3)
   final String author;
+  @HiveField(4)
   final String previewUrl;
+  @HiveField(5)
   final num pageCount;
 
   BookEntity({
