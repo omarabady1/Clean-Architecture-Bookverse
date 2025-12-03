d3a7a8b8d89a8636d01a2165eded919e69099264
omarabady <omarabady44@gmail.com>
2025-12-03 13:50:47 +0200

    Init Hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:bookverse/core/utils/app_router.dart';
 import 'package:bookverse/features/home/domain/entities/book_entity.dart';
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'constants.dart';
 
-void main() async{
+void main() async {
   runApp(const Bookverse());
+  await Hive.initFlutter();
   Hive.registerAdapter(BookEntityAdapter());
   await Hive.openBox(kFeaturedBox);
 }
