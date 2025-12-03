171ae881a10e1da3acd0792f7859fc90bf9acdcc
omarabady <omarabady44@gmail.com>
2025-12-03 13:14:42 +0200

    Create Home Remote Data Source

---
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -0,0 +1,38 @@
+import 'package:bookverse/core/utils/api_service.dart';
+import 'package:bookverse/features/home/data/models/book_model.dart';
+import 'package:bookverse/features/home/domain/entities/book_entity.dart';
+
+abstract class HomeRemoteDataSource {
+  Future<List<BookEntity>> fetchFeaturedBooks();
+  Future<List<BookEntity>> fetchNewestBooks();
+}
+
+class HomeRemoteDataSourceImp implements HomeRemoteDataSource {
+  final ApiService apiService;
+  HomeRemoteDataSourceImp(this.apiService);
+
+  @override
+  Future<List<BookEntity>> fetchFeaturedBooks() async{
+    var data = await apiService.get('volumes?q=subject:general');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  @override
+  Future<List<BookEntity>> fetchNewestBooks() async{
+    var data = await apiService.get('volumes?q=subject:Computers&orderBy=newest');
+    List<BookEntity> books = getBooksList(data);
+    return books;
+  }
+
+  List<BookEntity> getBooksList(Map<String, dynamic> data) {
+    List<BookEntity> books = [];
+    if (data['items'] != null && data['items'] is List) {
+      for(var item in data['items'])
+      {
+        books.add(BookModel.fromJson(item));
+      }
+    }
+    return books;
+  }
+}
