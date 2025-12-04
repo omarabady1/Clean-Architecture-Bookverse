d46153dce17bb651400cebc0eae72b64a50e75da
omarabady <omarabady44@gmail.com>
2025-12-04 18:17:56 +0200

    Create NewestBooks cubit

---
+++ lib/features/home/presentation/manager/newest_books_cubit.dart
@@ -0,0 +1,26 @@
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+import 'package:bookverse/features/home/domain/entities/book_entity.dart';
+import 'package:bookverse/features/home/domain/use_cases/fetch_newest_books_use_case.dart';
+// ignore: depend_on_referenced_packages
+import 'package:meta/meta.dart';
+
+part 'newest_books_state.dart';
+
+class NewestBooksCubit extends Cubit<NewestBooksState> {
+  NewestBooksCubit(this.fetchNewestBooksUseCase) : super(NewestBooksInitial());
+  final FetchNewestBooksUseCase fetchNewestBooksUseCase;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await fetchNewestBooksUseCase.call();
+    result.fold(
+      (failure) {
+        emit(NewestBooksFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(NewestBooksSuccess(books));
+      },
+    );
+  }
+}
