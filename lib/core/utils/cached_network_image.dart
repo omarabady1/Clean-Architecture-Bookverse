16b44086991941ec97b1a3a4c917d3213e575df9
omarabady <omarabady44@gmail.com>
2025-12-05 00:40:36 +0200

    Modify FeaturedBox thumbnail

--- lib/core/utils/cached_network_image.dart
+++ lib/core/utils/cached_network_image.dart
@@ -4,18 +4,27 @@ import 'package:cached_network_image/cached_network_image.dart';
 class CustomNetworkImage extends StatelessWidget {
   const CustomNetworkImage(this.imgUrl, {super.key});
 
-  final String imgUrl;
+  final String? imgUrl;
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadiusGeometry.circular(8),
-      child: CachedNetworkImage(
-        imageUrl: imgUrl,
-        placeholder: (context, _) => Center(
-          child: CircularProgressIndicator(backgroundColor: Color(0xffd65158)),
+    return AspectRatio(
+      aspectRatio: 75 / 112,
+      child: ClipRRect(
+        borderRadius: BorderRadiusGeometry.circular(8),
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl:
+              imgUrl ??
+              'https://thumbs.dreamstime.com/b/grunge-blue-not-available-word-round-rubber-seal-stamp-white-background-162738148.jpg?w=768',
+          placeholder: (context, _) => Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Color(0xffd65158),
+            ),
+          ),
+          errorWidget: (context, url, error) =>
+              Center(child: Icon(Icons.error)),
         ),
-        errorWidget: (context, url, error) => Center(child: Icon(Icons.error)),
       ),
     );
   }
